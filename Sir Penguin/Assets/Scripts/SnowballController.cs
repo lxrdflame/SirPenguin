c7d831e58cba14a2f3031205ed7a5d32aeb6dc40
lxrdflame <tyrique.s.r@gmail.com>
2026-02-24 11:49:07 +0200

    Sparks

--- Sir Penguin/Assets/Scripts/SnowballController.cs
+++ Sir Penguin/Assets/Scripts/SnowballController.cs
@@ -5,13 +5,30 @@ public class SnowballController : MonoBehaviour
     public Vector3 targetPoint;
     [SerializeField]
     private int moveSpeed;
-
+    [SerializeField] private GameObject SparkPrefab;
+    private bool atPoint;
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPoint, moveSpeed * Time.deltaTime);
         if (transform.position == targetPoint )
         {
             Destroy(gameObject, 3);
+            if (!atPoint )
+            {
+                atPoint = true;
+                GameObject spark = Instantiate(SparkPrefab, transform.position, Quaternion.identity);
+                Destroy(spark, 2);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null)
+        {
+            Destroy(gameObject, 3);
+            GameObject spark = Instantiate(SparkPrefab, transform.position, Quaternion.identity);
+            Destroy(spark, 2);
         }
     }
 }
