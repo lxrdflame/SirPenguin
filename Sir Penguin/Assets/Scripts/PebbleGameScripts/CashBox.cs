46edf90d4a7bcbed0b41a3bfa969b8fcc128db3a
profoundBoi <connor170103@gmail.com>
2026-03-06 00:26:28 +0200

    Point System working

--- Sir Penguin/Assets/Scripts/PebbleGameScripts/CashBox.cs
+++ Sir Penguin/Assets/Scripts/PebbleGameScripts/CashBox.cs
@@ -1,14 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CashBox : MonoBehaviour
 {
+    public int playerIndex;
     public float depositTime = 2f;
+    public TextMeshProUGUI scoreText;
+    private int totalDeposited = 0;
 
     private float currentTimer = 0f;
     private PlayerWallet currentPlayer;
 
     public void StartDeposit(PlayerWallet wallet)
     {
+        if (wallet.GetComponent<PlayerInput>().playerIndex != playerIndex)
+            return;
+
         if (currentPlayer != wallet)
         {
             currentPlayer = wallet;
@@ -17,13 +25,15 @@ public class CashBox : MonoBehaviour
 
         currentTimer += Time.deltaTime;
 
-        Debug.Log("Depositing: " + currentTimer);
-
         if (currentTimer >= depositTime)
         {
             int deposited = wallet.DepositAllPebbles();
-            Debug.Log(wallet.gameObject.name + " deposited: " + deposited);
+            totalDeposited += deposited;
 
+            if (scoreText != null)
+                scoreText.text = totalDeposited.ToString();
+
+            Debug.Log(wallet.gameObject.name + " deposited: " + deposited);
             currentTimer = 0f;
         }
     }
