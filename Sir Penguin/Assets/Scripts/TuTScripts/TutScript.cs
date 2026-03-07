89fd43b33c3c42e7b528a4dc5254442f5e121cba
profoundBoi <connor170103@gmail.com>
2026-03-08 00:39:55 +0200

    ALL3D ui done

--- Sir Penguin/Assets/Scripts/TuTScripts/TutScript.cs
+++ Sir Penguin/Assets/Scripts/TuTScripts/TutScript.cs
@@ -3,12 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class TutScript : MonoBehaviour
 {
+    public GameObject ControlsScreen;
+
+    private void Start()
+    {
+        if (ControlsScreen != null)
+        {
+            ControlsScreen.SetActive(false);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("SnowBall"))
+        if (!other.CompareTag("SnowBall")) return;
+
+        if (gameObject.CompareTag("StartTrigger"))
         {
             SceneManager.LoadScene("FreeForAll");
+        }
+
+        if (gameObject.CompareTag("ControlsTrigger"))
+        {
+            ControlsScreen.SetActive(true);
+        }
+
+        if (gameObject.CompareTag("ExitTrigger"))
+        {
+            ControlsScreen.SetActive(false);
+        }
+
+        if (gameObject.CompareTag("QuitTrigger"))
+        {
+            Application.Quit();
         }
     }
 }
