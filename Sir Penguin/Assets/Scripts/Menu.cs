90b15725fd7ee10f711a71a596ecd088d72c8bc9
profoundBoi <connor170103@gmail.com>
2026-03-09 13:11:44 +0200

    MenuUIDone

--- Sir Penguin/Assets/Scripts/Menu.cs
+++ Sir Penguin/Assets/Scripts/Menu.cs
@@ -1,19 +1,44 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject confirmationPanel;
+    public GameObject controlsPanel;
+    [SerializeField] private EventSystem eventSystem;
+    public GameObject tickGameObject;
+    public GameObject[] buttons;
+
     public void OnClickStart()
     {
+        confirmationPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(tickGameObject);
 
+        for (int i = 0; i < buttons.Length; i++) { 
+            buttons[i].SetActive(false);
+        }
+
+        
     }
 
     public void OnClickQuit()
     {
-
+        Application.Quit();
     }
 
     public void OnClickControls()
     {
+        confirmationPanel.SetActive(true);
+    }
 
+    public void OnClickTick()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void OnClickX()
+    {
+        SceneManager.LoadScene("FreeForAll");
     }
 }
