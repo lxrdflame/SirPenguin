ace82ca7c690fc45020f0e5a9b867ad48a7564a4
profoundBoi <connor170103@gmail.com>
2026-03-09 12:46:40 +0200

    TickPush

---
+++ Sir Penguin/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+
+    }
+
+    public void OnClickQuit()
+    {
+
+    }
+
+    public void OnClickControls()
+    {
+
+    }
+}
