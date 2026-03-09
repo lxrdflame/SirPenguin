73fa75e7b91ed4e30c595425b85b587c21f1eea9
profoundBoi <connor170103@gmail.com>
2026-03-09 16:23:09 +0200

    sound for menu

--- Sir Penguin/Assets/Scripts/Menu.cs
+++ Sir Penguin/Assets/Scripts/Menu.cs
@@ -11,26 +11,52 @@ public class Menu : MonoBehaviour
     public GameObject closeGameObject;
     public GameObject startGameObject;
     public GameObject[] buttons;
+    public AudioClip clickSound;
+    public AudioClip selectedSound;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void PlayClickSound()
+    {
+        if (clickSound != null)
+            audioSource.PlayOneShot(clickSound);
+    }
+
+    void PlaySelectedSound()
+    {
+        if (selectedSound != null)
+            audioSource.PlayOneShot(selectedSound);
+    }
 
     public void OnClickStart()
     {
+        PlayClickSound();
+        PlaySelectedSound();
+
         confirmationPanel.SetActive(true);
         eventSystem.SetSelectedGameObject(tickGameObject);
 
-        for (int i = 0; i < buttons.Length; i++) { 
+        for (int i = 0; i < buttons.Length; i++)
+        {
             buttons[i].SetActive(false);
         }
-
-        
     }
 
     public void OnClickQuit()
     {
+        PlayClickSound();
         Application.Quit();
     }
 
     public void OnClickControls()
     {
+        PlayClickSound();
+
         controlsPanel.SetActive(true);
         eventSystem.SetSelectedGameObject(closeGameObject);
 
@@ -42,16 +68,20 @@ public class Menu : MonoBehaviour
 
     public void OnClickTick()
     {
+        PlaySelectedSound();
         SceneManager.LoadScene("StartScreen");
     }
 
     public void OnClickX()
     {
+        PlaySelectedSound();
         SceneManager.LoadScene("FreeForAll");
     }
 
     public void OnClickClosePanel()
     {
+        PlayClickSound();
+
         controlsPanel.SetActive(false);
         eventSystem.SetSelectedGameObject(startGameObject);
 
@@ -63,6 +93,8 @@ public class Menu : MonoBehaviour
 
     public void OnClickBack()
     {
+        PlayClickSound();
+
         confirmationPanel.SetActive(false);
         eventSystem.SetSelectedGameObject(startGameObject);
 
